65cd34f5953a8aee06d437e62306477b5b91f7cd
John G. Levis Jr <jlevis@webitso.com>
2015-03-10 11:47:54 -0400

    Call back

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ define("CLIENT_ID", "3MVG9A2kN3Bn17hvZc9nekjyHNoZI732S5MWbq2kMdJDrpKZR5jjym37eEL
 
 define("CLIENT_SECRET", "4281366282980209317");
 
-define("REDIRECT_URI", "https://php-restbeta.herokuapp.com/oauth_callback.php");
+define("REDIRECT_URI", "https://fathomless-fjord-6605.herokuapp.com/oauth_callback.php");
 
 define("LOGIN_URI", "https://login.salesforce.com");
 
