4dcede7860417c2a468750ae625dadedf2053a37
John G. Levis Jr <jlevis@webitso.com>
2015-03-11 07:54:28 -0400

    Config_Push

--- layout.php
+++ layout.php
@@ -2,58 +2,229 @@
 //comment
 session_start();
 
- 
-
 function show_accounts($instance_url, $access_token) {
+	
+		$query = "SELECT Name, Id, AnnualRevenue FROM Account";
+		
+		$url = "$instance_url/services/data/v33.0/query?q=" . urlencode($query);
 
-    $query = "SELECT Name, Id, AnnualRevenue from Account LIMIT 250000";
+    	$curl = curl_init($url);
 
-    $url = "$instance_url/services/data/v20.0/query?q=" . urlencode($query);
+    	curl_setopt($curl, CURLOPT_HEADER, false);
 
- 
+    	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
-    $curl = curl_init($url);
+    	curl_setopt($curl, CURLOPT_HTTPHEADER,
 
-    curl_setopt($curl, CURLOPT_HEADER, false);
+        array("Authorization: OAuth $access_token"));
 
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    	$json_response = curl_exec($curl);
 
-    curl_setopt($curl, CURLOPT_HTTPHEADER,
+    	curl_close($curl);
 
-            array("Authorization: OAuth $access_token"));
+    	$response = json_decode($json_response, true);
 
- 
+    	$total_size = $response['totalSize'];		
+	
+	if(isset($_GET['pn'])){
+		$pn = preg_replace('#[^0-9]#i', '', $_GET['pn']); // filter everything but numbers for security(new)
+		//This is where we set how many database items to show on each page 
+$itemsPerPage = 10; 
 
-    $json_response = curl_exec($curl);
+// Get the value of the last page in the pagination result set
+$lastPage = ceil($total_size / $itemsPerPage);
 
-    curl_close($curl);
+// Be sure URL variable $pn(page number) is no lower than page 1 and no higher than $lastpage
+if ($pn < 1) { // If it is less than 1
+    $pn = 1; // force if to be 1
+} else if ($pn > $lastPage) { // if it is greater than $lastpage
+    $pn = $lastPage; // force it to be $lastpage's value
+} 
+		$offset = $pn * 10 - 10;
+		
+		$query = "SELECT Name, Id, AnnualRevenue FROM Account ORDER BY Id LIMIT 10 OFFSET $offset";
+		
+		$url = "$instance_url/services/data/v33.0/query?q=" . urlencode($query);
 
- 
+    	$curl = curl_init($url);
 
-    $response = json_decode($json_response, true);
+    	curl_setopt($curl, CURLOPT_HEADER, false);
 
- 
+    	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
-    $total_size = $response['totalSize'];
+    	curl_setopt($curl, CURLOPT_HTTPHEADER,
 
+        array("Authorization: OAuth $access_token"));
 
-    echo "<div class='container-fluid'><div class='bg-primary' align='center'><h2>$total_size record(s) returned</h2></div></div><br/><br/>
-    <div class='container'><div class='table-responsive'><table class='table'><tr><td width='33%'><h3>ID</h3></td><td width='33%'><h3>Name</h3></td><td width='33%'><h3>AnnualRevenue</h3></td></tr></table></br>";
+    	$json_response = curl_exec($curl);
 
+    	curl_close($curl);
 
-    foreach ((array) $response['records'] as $record) {
-
-        echo "<div class='container'><div class='table-responsive'><table class='table table-condensed table-hover'><tr><td width='33%'>".$record['Id']."</td><td width='33%'>".$record['Name']."</td><td width='33%'>$".$record['AnnualRevenue']."</td></tr></table></div></div>";
-
-    }
-
-    echo "<br/>";
-
+    	$response = json_decode($json_response, true);
+	
+		$records = $response['records'];
+		
+// This creates the numbers to click in between the next and back buttons
+// This section is explained well in the video that accompanies this script
+$centerPages = "";
+$sub1 = $pn - 1;
+$sub2 = $pn - 2;
+$add1 = $pn + 1;
+$add2 = $pn + 2;
+if ($pn == 1) {
+    $centerPages .= '&nbsp; <span class="pagNumActive">' . $pn . '</span> &nbsp;';
+    $centerPages .= '&nbsp; <a href="' . $_SERVER['PHP_SELF'] . '?pn=' . $add1 . '">' . $add1 . '</a> &nbsp;';
+} else if ($pn == $lastPage) {
+    $centerPages .= '&nbsp; <a href="' . $_SERVER['PHP_SELF'] . '?pn=' . $sub1 . '">' . $sub1 . '</a> &nbsp;';
+    $centerPages .= '&nbsp; <span class="pagNumActive">' . $pn . '</span> &nbsp;';
+} else if ($pn > 2 && $pn < ($lastPage - 1)) {
+    $centerPages .= '&nbsp; <a href="' . $_SERVER['PHP_SELF'] . '?pn=' . $sub2 . '">' . $sub2 . '</a> &nbsp;';
+    $centerPages .= '&nbsp; <a href="' . $_SERVER['PHP_SELF'] . '?pn=' . $sub1 . '">' . $sub1 . '</a> &nbsp;';
+    $centerPages .= '&nbsp; <span class="pagNumActive">' . $pn . '</span> &nbsp;';
+    $centerPages .= '&nbsp; <a href="' . $_SERVER['PHP_SELF'] . '?pn=' . $add1 . '">' . $add1 . '</a> &nbsp;';
+    $centerPages .= '&nbsp; <a href="' . $_SERVER['PHP_SELF'] . '?pn=' . $add2 . '">' . $add2 . '</a> &nbsp;';
+} else if ($pn > 1 && $pn < $lastPage) {
+    $centerPages .= '&nbsp; <a href="' . $_SERVER['PHP_SELF'] . '?pn=' . $sub1 . '">' . $sub1 . '</a> &nbsp;';
+    $centerPages .= '&nbsp; <span class="pagNumActive">' . $pn . '</span> &nbsp;';
+    $centerPages .= '&nbsp; <a href="' . $_SERVER['PHP_SELF'] . '?pn=' . $add1 . '">' . $add1 . '</a> &nbsp;';
+}
+		
+		//////Adam's Pagination Display Setup /////////////////////////////////////////////////////////////////////
+$paginationDisplay = ""; // Initialize the pagination output variable
+// This code runs only if the last page variable is ot equal to 1, if it is only 1 page we require no paginated links to display
+if ($lastPage != "1"){
+    // This shows the user what page they are on, and the total number of pages
+    $paginationDisplay .= '<div class="pagination">Page <strong>' . $pn . '</strong> of ' . $lastPage. '&nbsp;  &nbsp;  &nbsp; ';
+    // If we are not on page 1 we can place the Back button
+    if ($pn != 1) {
+        $previous = $pn - 1;
+        $paginationDisplay .=  '&nbsp;  <a href="' . $_SERVER['PHP_SELF'] . '?pn=' . $previous . '"> Back</a> ';
+    } 
+    // Lay in the clickable numbers display here between the Back and Next links
+    $paginationDisplay .= '<span class="paginationNumbers">' . $centerPages . '</span>';
+    // If we are not on the very last page we can place the Next button
+    if ($pn != $lastPage) {
+        $nextPage = $pn + 1;
+        $paginationDisplay .=  '&nbsp;  <a href="' . $_SERVER['PHP_SELF'] . '?pn=' . $nextPage . '"> Next</a></div>';
+    } 
 }
 
- 
+//////////////////////////////pagination/////////////////////////////////////////////////////////////////
+		
+		echo $paginationDisplay;
+		
+		$theDiv = "<br/><div class='container'><div class='table-responsive'><table class='table table-condensed table-hover'>";
+		
+		foreach ((array) $records as $record) {
+		
+        $theDiv .= "<tr><td width='33%'>".$record['Id']."</td><td width='33%'>".$record['Name']."</td><td width='33%'>$".$record['AnnualRevenue']."</td></tr>";
+    }
+		
+		$theDiv .= "</table></div></div>";
+	}else{
+		$pn = preg_replace('#[^0-9]#i', '', $_GET['pn']); // filter everything but numbers for security(new)
+    	$query = "SELECT Name, Id, AnnualRevenue FROM Account ORDER BY Id LIMIT 10 OFFSET 0";
+		$pn = 1;//set page number to 1 
+		
+				//This is where we set how many database items to show on each page 
+$itemsPerPage = 10; 
 
-function create_account($name, $instance_url, $access_token) {
+// Get the value of the last page in the pagination result set
+$lastPage = ceil($total_size / $itemsPerPage);
+
+// Be sure URL variable $pn(page number) is no lower than page 1 and no higher than $lastpage
+if ($pn < 1) { // If it is less than 1
+    $pn = 1; // force if to be 1
+} else if ($pn > $lastPage) { // if it is greater than $lastpage
+    $pn = $lastPage; // force it to be $lastpage's value
+} 
+		
+		$url = "$instance_url/services/data/v33.0/query?q=" . urlencode($query);
+
+    	$curl = curl_init($url);
+
+    	curl_setopt($curl, CURLOPT_HEADER, false);
+
+    	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+    	curl_setopt($curl, CURLOPT_HTTPHEADER,
+
+        array("Authorization: OAuth $access_token"));
+
+    	$json_response = curl_exec($curl);
+
+    	curl_close($curl);
+
+    	$response = json_decode($json_response, true);
+
+    	//$total_size = $response['totalSize'];
+	
+		$records = $response['records'];
+		
+
+// This creates the numbers to click in between the next and back buttons
+// This section is explained well in the video that accompanies this script
+$centerPages = "";
+$sub1 = $pn - 1;
+$sub2 = $pn - 2;
+$add1 = $pn + 1;
+$add2 = $pn + 2;
+if ($pn == 1) {
+    $centerPages .= '&nbsp; <span class="pagNumActive">' . $pn . '</span> &nbsp;';
+    $centerPages .= '&nbsp; <a href="' . $_SERVER['PHP_SELF'] . '?pn=' . $add1 . '">' . $add1 . '</a> &nbsp;';
+} else if ($pn == $lastPage) {
+    $centerPages .= '&nbsp; <a href="' . $_SERVER['PHP_SELF'] . '?pn=' . $sub1 . '">' . $sub1 . '</a> &nbsp;';
+    $centerPages .= '&nbsp; <span class="pagNumActive">' . $pn . '</span> &nbsp;';
+} else if ($pn > 2 && $pn < ($lastPage - 1)) {
+    $centerPages .= '&nbsp; <a href="' . $_SERVER['PHP_SELF'] . '?pn=' . $sub2 . '">' . $sub2 . '</a> &nbsp;';
+    $centerPages .= '&nbsp; <a href="' . $_SERVER['PHP_SELF'] . '?pn=' . $sub1 . '">' . $sub1 . '</a> &nbsp;';
+    $centerPages .= '&nbsp; <span class="pagNumActive">' . $pn . '</span> &nbsp;';
+    $centerPages .= '&nbsp; <a href="' . $_SERVER['PHP_SELF'] . '?pn=' . $add1 . '">' . $add1 . '</a> &nbsp;';
+    $centerPages .= '&nbsp; <a href="' . $_SERVER['PHP_SELF'] . '?pn=' . $add2 . '">' . $add2 . '</a> &nbsp;';
+} else if ($pn > 1 && $pn < $lastPage) {
+    $centerPages .= '&nbsp; <a href="' . $_SERVER['PHP_SELF'] . '?pn=' . $sub1 . '">' . $sub1 . '</a> &nbsp;';
+    $centerPages .= '&nbsp; <span class="pagNumActive">' . $pn . '</span> &nbsp;';
+    $centerPages .= '&nbsp; <a href="' . $_SERVER['PHP_SELF'] . '?pn=' . $add1 . '">' . $add1 . '</a> &nbsp;';
+}
+		
+		//////Adam's Pagination Display Setup /////////////////////////////////////////////////////////////////////
+$paginationDisplay = ""; // Initialize the pagination output variable
+// This code runs only if the last page variable is ot equal to 1, if it is only 1 page we require no paginated links to display
+if ($lastPage != "1"){
+    // This shows the user what page they are on, and the total number of pages
+    $paginationDisplay .= '<div class="pagination">Page <strong>' . $pn . '</strong> of ' . $lastPage. '&nbsp;  &nbsp;  &nbsp; ';
+    // If we are not on page 1 we can place the Back button
+    if ($pn != 1) {
+        $previous = $pn - 1;
+        $paginationDisplay .=  '&nbsp;  <a href="' . $_SERVER['PHP_SELF'] . '?pn=' . $previous . '"> Back</a> ';
+    } 
+    // Lay in the clickable numbers display here between the Back and Next links
+    $paginationDisplay .= '<span class="paginationNumbers">' . $centerPages . '</span>';
+    // If we are not on the very last page we can place the Next button
+    if ($pn != $lastPage) {
+        $nextPage = $pn + 1;
+        $paginationDisplay .=  '&nbsp;  <a href="' . $_SERVER['PHP_SELF'] . '?pn=' . $nextPage . '"> Next</a></div>';
+    } 
+}
+
+//////////////////////////////pagination/////////////////////////////////////////////////////////////////
+		
+		echo $paginationDisplay;
+		
+		$theDiv = "<div class='container'><div class='table-responsive' style='overflow: hidden;'><table class='table table-condensed table-hover'>";
+		
+		foreach ((array) $records as $record) {
+		
+        $theDiv .= "<tr><td width='33%'>".$record['Id']."</td><td width='33%'>".$record['Name']."</td><td width='33%'>$".$record['AnnualRevenue']."</td></tr>";
+    }	
+	$theDiv .= "</table></div></div>";
+	}
+
+    echo "<div class='container-fluid'><div class='bg-primary' align='center'><h2>Total Number Of Records: $total_size</h2></div></div><br/><br/>
+    <div class='container'><div class='table-responsive' style='overflow: hidden;'><table class='table'><tr><td width='33%'><h3>ID</h3></td><td width='33%'><h3>Name</h3></td><td width='33%'><h3>AnnualRevenue</h3></td></tr></table>";	
+	echo $theDiv;
+}
+/*function create_account($name, $instance_url, $access_token) {
 
     $url = "$instance_url/services/data/v20.0/sobjects/Account/";
 
@@ -268,7 +439,7 @@ function delete_account($id, $instance_url, $access_token) {
 
     curl_close($curl);
 
-}
+}*/
 
 ?>
 
@@ -288,9 +459,7 @@ function delete_account($id, $instance_url, $access_token) {
 
     <body>
 
-   
-
-            <?php
+			<?php
 
             $access_token = $_SESSION['access_token'];
 
@@ -318,7 +487,7 @@ function delete_account($id, $instance_url, $access_token) {
 
  
 
-            $id = create_account("My New Org", $instance_url, $access_token);
+            /*$id = create_account("My New Org", $instance_url, $access_token);
 
  
 
@@ -348,11 +517,12 @@ function delete_account($id, $instance_url, $access_token) {
 
  
 
-            show_accounts($instance_url, $access_token);
+            show_accounts($instance_url, $access_token);*/
 
             ?>
 
-    
+
+
  <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
