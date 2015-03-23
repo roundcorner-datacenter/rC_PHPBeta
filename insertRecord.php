b6dc4d7a39500e09b9ceb57930c2045e448daa04
John G. Levis Jr <jlevis@webitso.com>
2015-03-23 06:26:28 -0400

    stlye

--- insertRecord.php
+++ insertRecord.php
@@ -43,6 +43,9 @@ $conn->close();
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
 <title>Insert Record</title>
 </head>
 
